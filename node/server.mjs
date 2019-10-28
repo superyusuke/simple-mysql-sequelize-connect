59761e0501b9aa64737069c6782133e21e4776e2
Nakanishi, Yusuke <nakanishiyusuke0000@gmail.com>
2019-10-28 15:10:08 +0900

    とりあえず db にコネクトして初期化できた。

--- node/server.mjs
+++ node/server.mjs
@@ -1,4 +1,7 @@
 import restify from "restify";
+import * as usersModel from "./sequelize.mjs";
+import DBG from "debug";
+const log = DBG("users:model-users");
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,6 +13,20 @@ function respond(req, res, next) {
 const server = restify.createServer();
 server.get("/hello/:name", respond);
 server.head("/hello/:name", respond);
+
+server.get("/list", async (req, res, next) => {
+  res.send('just list')
+  try {
+    var res = await usersModel.justDBConnect();
+    log('after db connect')
+    res.send("in try");
+    next(false)
+  } catch (err) {
+    log(err)
+    res.send(err);
+    next(false)
+  }
+});
 
 server.listen(PORT, function() {
   console.log("%s listening at %s", server.name, server.url);
